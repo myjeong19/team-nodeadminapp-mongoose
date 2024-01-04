e24c15efffa537859827101cb3c8d109d57748bf
ysungwon <hwoarang09@naver.com>
2024-01-04 21:49:55 +0900

    router error fix

--- routes/message.js
+++ routes/message.js
@@ -5,6 +5,13 @@ var moment = require("moment");
 const ChannelMessage = require("../schemas/channelMessage");
 const Swal = require("sweetalert2");
 
+const mergeByKey = (baseObj, otherObj) => {
+  return Object.keys(baseObj).reduce((result, key) => {
+    result[key] = otherObj[key];
+    return result;
+  }, baseObj);
+};
+
 router.get("/list", async (req, res) => {
   var searchOption = {
     channel_id: "",
@@ -26,6 +33,23 @@ router.post("/list", async (req, res) => {
     nick_name,
     msg_type_code,
   };
+
+  //reduce는 array를 순환하면서 하나의 무언가로 바꾸는 기능
+  // [1,2,3,4]를 reduce를 통해 10으로 return할수도 있고
+  // {channel_id : '삼성', nick_name : '성원', msg_type_code : ""}를
+  //중간에 if문 걸어서 ""인 경우는 제외한 object를 리턴하는 것도 가능.
+  //{channel_id : '삼성', nick_name : '성원'}
+  //map, filter를 array를 리턴하지만,
+  //reduce는 리턴값이 자유로움.
+
+  //이번 경우에는 searchOption이라는 객체에서
+  //key array를 통해 접근해서
+  //다른 -객체-를 리턴해야하니까 reduce를 사용함.
+
+  //reduce는 Array.reduce( ()=> {}, {})형식임.
+  //첫번째 인자는 원소마다 적용할 콜백함수,
+  //두번째 인자는 리턴값의 초기설정값.
+
   const filteredObject = Object.keys(searchOption).reduce((acc, key) => {
     if (searchOption[key] !== "") {
       acc[key] = searchOption[key];
@@ -77,38 +101,60 @@ router.post("/create", async (req, res) => {
 
 router.get("/modify/:channel_msg_id", async (req, res) => {
   const channel_msg_id = req.params.channel_msg_id;
-  if (channel_msg_id === undefined) res.send("error");
-  else {
+  console.log(channel_msg_id);
+  try {
     const msg = await ChannelMessage.findOne({ channel_msg_id });
-
-    res.render("message/modify", { msg });
+    if (msg === null) res.send("no channel_msg_id : " + channel_msg_id);
+    else res.render("message/modify", { msg });
+  } catch (err) {
+    console.log("err", err);
+    if (channel_msg_id === undefined || channel_msg_id === "")
+      res.send("channel_msg_id need! " + err);
+    else res.send("error!!! : " + err);
   }
 });
 
 router.post("/modify/:channel_msg_id", async (req, res) => {
   var channel_msg_id = req.params.channel_msg_id;
-  if (channel_msg_id === undefined) res.send("error!");
-  else {
+
+  try {
     const msg = await ChannelMessage.findOne({ channel_msg_id });
-    msg.channel_id = Number(req.body.channel_id);
-    msg.member_id = Number(req.body.member_id);
-    msg.nick_name = req.body.nick_name;
-    msg.msg_type_code = req.body.msg_type_code;
-    msg.message = req.body.message;
-    msg.connection_id = req.body.connection_id;
-    msg.ip_address = req.body.ip_address;
-    msg.msg_state_code = req.body.msg_state_code;
 
-    msg.edit_date = Date.now();
-
-    console.log("modify post message : ", JSON.stringify(msg, null, 2));
-    await ChannelMessage.updateOne({ channel_msg_id: channel_msg_id }, msg);
+    mergedObject = mergeByKey(msg.toJSON(), req.body);
+    mergedObject.edit_date = Date.now();
+    await ChannelMessage.updateOne({ channel_msg_id }, mergedObject);
     res.redirect("/message/list");
+  } catch (err) {
+    if (channel_msg_id === undefined || channel_msg_id === "")
+      res.send("channel_msg_id need! " + err);
+    else res.send("error!!! : " + err);
   }
 });
 
 router.get("/delete", async (req, res) => {
   res.redirect("list");
+});
+
+//아래의 에러처리 코드는 무조건 router정의가 다 끝난 최하단에 위치해야 함.
+
+//위에서 정의하지 않은 라우터에 대한 모든 요청에 대해서
+//Error 객체를 생성하는 아래의 미들웨어를 실행한다.
+
+router.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+//위에서 받은 Error객체를 통해 화면에 처리하는 미들웨어
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
 });
 
 module.exports = router;
