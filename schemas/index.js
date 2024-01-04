2db7f5cb5d7be574fce792ba34ffc7e5377df0b3
myjeong19@naver.com <myjeong19@naver.com>
2024-01-04 14:02:59 +0900

    ODM channel

--- schemas/index.js
+++ schemas/index.js
@@ -32,5 +32,6 @@ mongoose.connection.on('disconnected', () => {
 require('./article.js');
 require('./member.js');
 require('./admin_member.js');
+require('./channel.js');
 
 module.exports = connect;
